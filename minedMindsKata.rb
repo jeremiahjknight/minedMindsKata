b0fc4a250ac1c782a1b2ea968106a0ce842d701d
jeremiahjknight <jeremiahjknight@gmail.com>
2017-06-28 11:27:03 -0400

    Stage Four

--- minedMindsKata.rb
+++ minedMindsKata.rb
@@ -22,10 +22,32 @@ end
 =end
 
 #Stage Three - Output 0-100 with "mined" on numbers divisible by 3 and "minds" on numbers divisible by 5
+=begin
 i = 0;
 while i <= 100
 	# "mined" will trigger first on numbers divisible by 5 (This is intentional for this stage)
 	if i %3 == 0 && i != 0
+		puts "mined"
+		i+=1
+	elsif i %5 == 0 && i != 0
+		puts "minds"
+		i+=1		
+	else
+		puts i
+		i+=1
+	end
+end
+=end
+
+#Stage Four - Output 0-100 with "mined" on numbers divisible by 3 and "minds" on numbers divisible by 5 and "minedminds" on overlap
+i = 0;
+while i <= 100
+	# corrects previous stage (line49) behavior
+	if i %3 == 0 && i %5 == 0 && i != 0
+		puts "minedminds"
+		i+=1
+	# "mined" will trigger first on numbers divisible by 5 (This is intentional for this stage)
+	elsif i %3 == 0 && i != 0
 		puts "mined"
 		i+=1
 	elsif i %5 == 0 && i != 0
