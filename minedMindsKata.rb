13e1a43ca06c57092e303bbbde01cd9eb4172924
jeremiahjknight <jeremiahjknight@gmail.com>
2017-06-28 11:10:26 -0400

    Stage Two

--- minedMindsKata.rb
+++ minedMindsKata.rb
@@ -1,6 +1,20 @@
 #Stage One - Output 0-100
+=begin
 i = 0;
 while i <= 100
 	puts i
 	i+=1
+end
+=end
+
+#Stage Two - Output 0-100 with "mined" on numbers divisible by 3
+i = 0;
+while i <= 100
+	if i %3 == 0 && i != 0
+		puts "mined"
+		i+=1
+	else
+		puts i
+		i+=1
+	end
 end
